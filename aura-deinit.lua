4ef98e0f2164350957db0b2ed6dd33b55c3e3413
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2020-05-22 00:49:48 +0300

    Moved tooltip hide operation to trigger (in GROUP_LEFT event)

--- aura-deinit.lua
+++
@@ -1,7 +0,0 @@
-for aura_name, _ in pairs(aura_env.runtime.config) do
-    if aura_env.helpers.AuraIsInDebug() then
-        print('Hidding tooltip frame for '..aura_name)
-    end
-
-    aura_env.runtime.helpers.TooltipHide(aura_name)
-end
--- aura-trigger.lua
+++ aura-trigger.lua
@@ -115,6 +115,15 @@ function(allstates, event, unit)
         
         return true
     end
+    if event == 'GROUP_LEFT' then
+        for aura_name, _ in pairs(aura_env.runtime.config) do
+            if aura_env.helpers.AuraIsInDebug() then
+                print('Hidding tooltip frame for '..aura_name)
+            end
+        
+            aura_env.runtime.helpers.TooltipHide(aura_name)
+        end
+    end
     if event == 'PLAYER_REGEN_DISABLED' then
         if aura_env.helpers.AuraIsInDebug() then
             print('TRIGGER: PLAYER_REGEN_DISABLED')
