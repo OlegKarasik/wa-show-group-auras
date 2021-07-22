a58273685b4b576dab6ad8fe1cde69748db1593e
meatGUY <karasik.oleg.nikolaevich@gmail.com>
2021-07-22 22:30:13 +0300

    updated version

--- aura-init.lua
+++ aura-init.lua
@@ -850,7 +850,7 @@ local loc_a = aura_env.config.localization.auras[locale]
 local loc_s = aura_env.config.localization.strings[locale]
 
 if aura_env.helpers.AuraIsInDebug() then
-    print('Aura version: 1.1.3')
+    print('Aura version: 1.1.4')
 end
 
 for _, aura_config in ipairs(aura_env.config.auras) do
